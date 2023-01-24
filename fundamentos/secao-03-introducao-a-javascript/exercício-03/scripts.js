35ec5a6f88b5fc760f7fdfa78cca3547d7ded942
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-01-24 19:46:07 -0300

    terminando o dia, atividades parcialmente realizadas

---
+++ fundamentos/secao-03-introducao-a-javascript/exercício-03/scripts.js
@@ -0,0 +1,58 @@
+/*let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index = index += 1) {
+    console.log(groceryList[index]);
+}*/
+
+/*let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let item of names) {
+    console.log(item);
+}*/
+
+//iniciando os exercícios foguetinho!
+
+//percorre a array e print os itens
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+}*/
+
+//aqui eu to somando todos os itens do array
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    result += (numbers[index]);
+}
+
+console.log(result);*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/*let parcial = 0; //tive que definir uma nova var
+
+for (let index = 0; index < numbers.length; index += 1) {
+    parcial += (numbers[index]); //parcial vai ser: 5, 5+9, 5+9+3...
+}
+let media = parcial / numbers.length;
+
+if (media > 20) {
+    console.log('Valor maior do que 20.')
+} else {
+    console.log('Valor diferente de 20.')
+}*/
+
+/*let maiorValor = numbers[0]; //aqui o meu maiorValor recebe 5.
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorValor) { //aqui eu vou comparar numero a numero, SE for maior, eu atribuo novo valor ao maiorValor
+        maiorValor = numbers[index];
+    }
+}
+
+console.log(maiorValor);*/
