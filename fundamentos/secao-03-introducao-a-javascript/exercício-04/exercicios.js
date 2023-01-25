50ecd4744d2ac2e758b597c5200bade392a27880
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-01-25 18:01:06 -0300

    aula ao vivo

---
+++ fundamentos/secao-03-introducao-a-javascript/exercício-04/exercicios.js
@@ -0,0 +1,24 @@
+// 1 - 
+// Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+//A soma total de 1 a 50 é:
+
+/*let soma = 0; 
+
+for (let index = 1; index <= 50; index += 1) {
+    soma += index;
+}
+
+console.log('A soma total é: ' + soma);*/
+
+/*let divisiveis = [];
+
+for (let index = 2; index <= 150; index += 1) {
+    if (index % 3 === 0) {
+        divisiveis.push(index);
+    }
+}
+if (divisiveis.length === 50) {
+    console.log('msg secreta')
+}*/
+
+
---
+++ fundamentos/secao-03-introducao-a-javascript/exercício-04/scripts.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let index2 = 0; index2 < index; index += 1) {
+        if (numbers[index] < numbers[index2]) {
+            let position = numbers[index];
+            numbers[index] = numbers[index2];
+            numbers[index2] = position;
+        }
+    }
+}
+
+console.log(numbers);
