76cffb2e1799bbe379f35ce388693eebe4874e9d
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-01-27 11:41:05 -0300

    iniciando o conteúdo de functions

--- fundamentos/secao-03-introducao-a-javascript/exercício-04/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/exercício-04/scripts.js
@@ -1,27 +1,11 @@
-//fatorial de 10
+//imprime asteriscos com  o tamanho de 'n'
 
-// !3 = 3*2*1
+let num = 5;
+let symbol = '*'
+let asteriscos = '';
 
-//criar um cod que multiplique o maior numero por ele -1, até o limite de 1, pq zero buga a mult
-
-/*let resultado = 1;
-
-for (let index = 10; index > 1; index -= 1) {
-    resultado *= index;
-} 
-
-console.log(resultado);*/
-
-// inverter uma palvra
-
-// eu quero percorrer a string e realocar os caracteres de trás p frente
-
-// ultimo valor da string, quero mudar a posição para a primeira
-
-let word = 'tryber';
-let newWord = '';
-
-for (index = word.length; index > word.length; index -= 1) {
-    newWord = console.log(word[index]);
-    //newWord = word[index];
+for (let index = 1; index <= num; index += 1) {
+    asteriscos = asteriscos + symbol
 }
+
+console.log(asteriscos);
