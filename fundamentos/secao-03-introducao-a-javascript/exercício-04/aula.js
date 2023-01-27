526c75c447a6931824fa5ae923d0f0fb9fd5a762
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-01-26 23:12:24 -0300

    termino do dia letivo

--- fundamentos/secao-03-introducao-a-javascript/exercício-04/aula.js
+++ fundamentos/secao-03-introducao-a-javascript/exercício-04/aula.js
@@ -1,4 +1,4 @@
-let player = {
+/*let player = {
     name: 'Marta',
     lastName: 'Silva',
     age: 34,
@@ -13,4 +13,31 @@ console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + play
 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes, nos anos de: ' + player.bestInTheWorld);
 
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');*/
+
+/*let food = ['hamburguer', 'bife', 'acarajé'];
+
+for (let value of food) {
+  console.log(value);
+};
+//resultado: hamburguer, bife, acarajé;*/
+
+/*let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+for (let index in names) {
+    console.log('Olá ' + names[index]);
+}*/
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+for (let key in car) {
+    console.log(key , car[key])
+}
