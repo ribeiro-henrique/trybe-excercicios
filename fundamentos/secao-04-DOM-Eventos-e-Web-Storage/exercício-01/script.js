60b75c3fcd7c02f28c92be09099b2a0d4ce5f66a
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-02-01 11:14:56 -0300

    exercicio basico feito

---
+++ fundamentos/secao-04-DOM-Eventos-e-Web-Storage/exercício-01/script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("#header-container");
+header.style.backgroundColor = 'green'
+
+const emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasks.style.backgroundColor = 'purple'
+
+const noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks')[0];
+noEmergencyTasks.style.backgroundColor = 'pink' 
+
+const footerContainer = document.getElementById('footer-container');
+footerContainer.style.backgroundColor = 'aquamarine'
